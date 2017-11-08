f9c7964dde349ec327622e999e1d8f89a66d1bc4
redahmeid <reda@redahmeid.com>
2017-11-08 18:04:09 +0000

    get email from token

--- src/main/java/io/openlight/lambda/GetBookHandler.java
+++ src/main/java/io/openlight/lambda/GetBookHandler.java
@@ -33,8 +33,8 @@ public class GetBookHandler implements RequestHandler<APIGatewayProxyRequestEven
             DecodedJWT jwt = JWT.decode(token);
 
             System.out.println(jwt.getClaims());
-            System.out.println(jwt.getClaims().get("email_verified"));
-            System.out.println(jwt.getClaims().get("cognito:username").isNull());
+            System.out.println(jwt.getClaims().get("email").asString());
+            System.out.println(jwt.getClaims().get("cognito:username").asString());
         } catch (JWTDecodeException exception){
             exception.printStackTrace();
         }
