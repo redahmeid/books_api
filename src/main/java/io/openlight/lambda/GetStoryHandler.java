ec899639d914b6420ecf9f6a4ee67c56f12ca23f
redahmeid <reda@redahmeid.com>
2018-01-04 10:30:00 +0000

    build error

--- src/main/java/io/openlight/lambda/GetStoryHandler.java
+++ src/main/java/io/openlight/lambda/GetStoryHandler.java
@@ -26,48 +26,8 @@ public class GetStoryHandler extends AbstractLambda {
     public APIGatewayProxyResponseEvent handle(APIGatewayProxyRequestEvent input, Context context, User user) {
         String chapterId = input.getPathParameters().get("chapterid");
 
-        Response response = new Response();
-
-        DomainResponse domainResponse = ChapterFinder.getById(chapterId);
-        Chapter chapter = (Chapter)domainResponse.data;
-
-        response.self = "http://sandbox.api.openlight.io/books/"+chapter.book +"/chapters/"+domainResponse.id;
-
-        // add some potential actions
-        if(BookFinder.getById(chapter.book).get().data.editor.equals(user.username)){
-            System.out.println("User is the editor of this book.");
-            Link link = new Link();
-            link.url = response.self+"/select";
-            link.rel = "select_this_chapter";
-            response.addAction(link);
-        }
-
-        Link alternativeLink = new Link();
-        alternativeLink.url = "http://sandbox.api.openlight.io/books/"+chapter.book+"/chapters";
-        alternativeLink.rel = "propose_an_alternative_chapter";
-        response.addAction(alternativeLink);
-
-        chapter.book = "http://sandbox.api.openlight.io/books/"+chapter.book;
-        chapter.writer = "http://sandbox.api.openlight.io/users/"+chapter.writer;
-        chapter.previous = null;
-
-        Link previousLink = new Link();
-        previousLink.url = chapter.book +"/chapters/"+chapter.previous;
-        previousLink.rel = "previous";
-        response.addRelated(previousLink);
 
 
-        response.data = chapter;
-
-        Link link = new Link();
-        link.url = chapter.book +"/chapters/proposed";
-        link.rel = "other_proposed_chapters_for_book";
-        response.addRelated(link);
-        String responseJson = gson.toJson(response);
-
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", MediaTypes.CHAPTER.type());
-
-        return new APIGatewayProxyResponseEvent().withBody(responseJson).withHeaders(headers).withStatusCode(200);
+        return new APIGatewayProxyResponseEvent().withStatusCode(200);
     }
 }
